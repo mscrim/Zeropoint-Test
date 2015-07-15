6c3dd2863dc154379b25323504af1bd7555aaaed
Morag Scrimgeour <morag.astro@gmail.com>
2015-07-15 23:00:39 +0800

    Modified ZeropointTest.py

--- ZeropointTest.py
+++ ZeropointTest.py
@@ -28,6 +28,28 @@ def SS_BF(g):
                   np.sum(wz*g[:,6])])
     return u
 
+def MLE_BF(g):
+    ngal = len(g)
+    x_arr = g[:,0]
+    y_arr = g[:,1]
+    z_arr = g[:,2]
+    r_arr = g[:,3]
+    v_arr = g[:,6]
+    sigmastar2 = 250.**2
+    sigma_n = np.zeros(ngal)
+
+    Aij = np.zeros([3,3])
+    rihat = np.array([x_arr/r_arr, y_arr/r_arr, z_arr/r_arr])
+    for i in range(3):
+        for j in range(3):
+            Aij[i,j] = np.sum(rihat[i,n]*rihat[j,n] / (sigma_n[n]**2. + sigmastar2) for n in range(ngal))
+    Aij_inv = np.linalg.pinv(Aij)
+    rsum = np.array([np.sum(rihat[0,n]*v_arr[n]/(sigma_n[n]**2.+sigmastar2) for n in range(ngal)),
+                     np.sum(rihat[1,n]*v_arr[n]/(sigma_n[n]**2.+sigmastar2) for n in range(ngal)),
+                     np.sum(rihat[2,n]*v_arr[n]/(sigma_n[n]**2.+sigmastar2) for n in range(ngal))])
+    u = np.dot(Aij_inv,rsum)
+    return u
+
 def mag(vector):
     if len(vector) != 3: raise Exception('mag function only accepts 3D vector')
     result = np.sqrt(vector[0]**2 + vector[1]**2 + vector[2]**2)
@@ -48,18 +70,24 @@ def x_to_v(x, z_obs, Dz, zfile):
     v = speedlight * ((z_obs - zr_der)/(1.+zr_der))
     return v
 
+def meanVector(tuplelist):
+    meanvec = [np.mean(y) for y in zip(*tuplelist)]
+    return meanvec
+
 def linearVelocityTest(mocksFolder):
     ''' Test Normalising Velocities to Zero '''
 
     vArr = []
-    diff = []
+    magArr = []
+    BFdiffArr = []
+    dmagArr = []
     # Read in each mock. 
     for i in range(20):
         mockFile = '%svmock_Gz_highbias_c0p6_%s.dat' % (mocksFolder,str(i).zfill(2))
         data = np.loadtxt(mockFile, comments='#')
         g = data[:,[3,4,5,6,7,8,11]]
         # Calculate true SSBF
-        trueBF = SS_BF(g)
+        trueBF = MLE_BF(g)
         # Find galaxies within -20 < Dec < 0
         gInC = g[g[:,5]>-20]
         # Calculate mean radial velocity within -20 < Dec < 0 circle
@@ -67,20 +95,38 @@ def linearVelocityTest(mocksFolder):
         # Renormalise all velocities in mock to normalise Great Circle to zero
         g[:,6] -= v
         # New SSBF
-        newBF = SS_BF(g)
+        newBF = MLE_BF(g)
+        BFdiff = newBF-trueBF
         print 'Old BF: ', trueBF, ', New BF: ', newBF
-        print 'Change in BF: ', mag(newBF - trueBF)
-        diff.append(mag(newBF - trueBF))
+        print 'Change in BF: ', mag(BFdiff)
+        dmag = mag(newBF) - mag(trueBF)
+        dmagArr.append(dmag)
+        magArr.append(mag(BFdiff))
+        BFdiffArr.append((BFdiff[0],BFdiff[1],BFdiff[2]))
         vArr.append(v)
+
     vArr = np.array(vArr)
-    diff = np.array(diff)
+    magArr = np.array(magArr)
+    dmagArr = np.array(dmagArr)
+
+    meanBFdiff = meanVector(BFdiffArr)
 
     print 'Mean radial v in circle: ', np.mean(vArr)
     print 'Variance: ',np.var(vArr)
     print 'Std: ', np.std(vArr)
+    print ''
 
-    print 'Mean BF diff: ', np.mean(diff)
-    print 'Std: ', np.std(diff)
+    print 'Mean BF diff mag: ', np.mean(magArr)
+    print 'Std: ', np.std(magArr)
+    print ''
+
+    print 'Mean BF diff: ', meanBFdiff
+    print 'Mag: ', mag(meanBFdiff)
+    print ''
+
+    print 'Mean dmag: ', np.mean(dmagArr)
+    print 'Std: ', np.std(dmagArr)
+
     return
 
 def observableEtaTest(mocksFolder):
@@ -88,19 +134,21 @@ def observableEtaTest(mocksFolder):
 
     zfile = '/Users/Morag/Cosmology_codes/redshift_OM0.3175.dat'
 
-    vArr = []
-    diff = []
+    xArr = []
+    magArr = []
+    BFdiffArr = []
     # Read in each mock. 
+    #  r  s  i  x[Mpc/h]  y[Mpc/h]  z[Mpc/h]  D_c[Mpc/h]  RA  Dec  ztrue  zobs  vpec[km/s]  appJ  V_max_sub
     for i in range(20):
         mockFile = '%svmock_Gz_highbias_c0p6_%s.dat' % (mocksFolder,str(i).zfill(2))
         data = np.loadtxt(mockFile, comments='#')
-        # x[Mpc/h]  y[Mpc/h]  z[Mpc/h]  D_c[Mpc/h]  RA  Dec  zobs vpec[km/s]
-        g = data[:,[3,4,5,6,7,8,10,11]]
+        # x[Mpc/h]  y[Mpc/h]  z[Mpc/h]  D_c[Mpc/h]  RA  Dec vpec[km/s] zobs
+        g = data[:,[3,4,5,6,7,8,11,10]]
         # Calculate true SSBF
-        trueBF = SS_BF(g)
+        trueBF = MLE_BF(g)
 
         # Calulate observable x 
-        x = v_to_x(g[:,7], g[:,6], g[:,3])
+        x = v_to_x(g[:,6], g[:,7], g[:,3])
         # Append x values to g
         g = np.column_stack((g, x[:,None]))
 
@@ -108,19 +156,41 @@ def observableEtaTest(mocksFolder):
         gInC = g[g[:,5]>-20]
         # Calculate mean radial velocity within -20 < Dec < 0 circle
         meanx = np.mean(gInC[:,8])
-
+        
         # Renormalise all x values in mock to normalise Great Circle to zero
         g[:,8] -= meanx
 
         # Convert these back to velocities
-        g[:,7] = 
+        OM = 0.31
+        Dz = cc.dxx_mpch_arr(len(g), g[:,7], OM)
+        g[:,6] = x_to_v(g[:,8], g[:,7], Dz, zfile)
 
+        # Calculate true SSBF
+        newBF = MLE_BF(g)
 
+        BFdiff = newBF-trueBF
+        print 'Old BF: ', trueBF, ', New BF: ', newBF
+        print 'Change in BF: ', mag(BFdiff)
+        magArr.append(mag(BFdiff))
+        BFdiffArr.append((BFdiff[0],BFdiff[1],BFdiff[2]))
+        xArr.append(meanx)
 
+    xArr = np.array(xArr)
+    magArr = np.array(magArr)
 
-        raise Exception('debug')
+    meanBFdiff = meanVector(BFdiffArr)
 
-        #gInC = g[g[:,5]>-20]
+    print 'Mean radial x in circle: ', np.mean(xArr)
+    print 'Variance: ',np.var(xArr)
+    print 'Std: ', np.std(xArr)
+    print ''
+
+    print 'Mean BF diff mag: ', np.mean(magArr)
+    print 'Std: ', np.std(magArr)
+    print ''
+
+    print 'Mean BF diff: ', meanBFdiff
+    print 'Mag: ', mag(meanBFdiff)
         
     return
 
@@ -130,10 +200,10 @@ def main():
     mocksFolder = '/Users/Morag/6dFGS/Mock_code/mocks/'
 
     ''' Test Normalising Velocities to Zero '''
-    #linearVelocityTest(mocksFolder)
+    linearVelocityTest(mocksFolder)
 
     ''' Test Normalising Observed eta to Zero '''
-    observableEtaTest(mocksFolder)
+    #observableEtaTest(mocksFolder)
 
 
 
